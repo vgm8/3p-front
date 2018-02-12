c0cd5ddc7276becb905287d3cfd9cd8dbdbf6732
victor_gilm <victor.gil@geniac.com>
2018-02-12 17:21:20 +0100

    :tada: Casos versión 1

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Home } from './components/Home.js';
+import { About } from './components/About.js';
+import { Contact } from './components/Contact.js';
+import { Navbar } from './components/Navbar/Navbar.js';
+import { HashRouter as Router, Redirect, Switch, Route, NavLink, Link } from 'react-router-dom';
+import { Casos } from './components/Casos/Casos.js';
+import { CreateCases } from './components/Casos/CreateCases.js';
+// import './App.css';
+
+// const routes = [
+//   { path: '/home',
+//     component: Home
+//   },
+//   { path: '/casos',
+//     component: Casos,
+//     routes: [
+//       { path: '/casos/todos',
+//         component: Casos,
+//       },
+//       { path: '/casos/borradores',
+//         component: Casos
+//       }
+//     ]
+//   }
+// ]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home}/>
+                <Redirect exact path="/casos" to="/casos/todos" />
+                <Route path="/casos" component={Casos}/>
+                <Route path="/create-case" component={CreateCases}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+require('../dist/index.html');
+import './index.styl';
+import './scss/main.scss';
+// import { Navbar } from './components/Navbar.js';
+import App from './App';
+
+// ReactDOM.render(<Navbar />, document.getElementById('react-nav'));
+ReactDOM.render(<App />, document.getElementById('react-container'));
